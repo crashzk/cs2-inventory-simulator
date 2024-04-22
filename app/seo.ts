2ebed7048f01ce5af4aea414e587ba240a7b1886
crashzk <contato@zkservidores.com>
2024-04-21 21:26:14 -0300

    update: SEO

--- app/seo.ts
+++ app/seo.ts
@@ -17,7 +17,7 @@ export function getSeoMeta({
 }) {
   const appDescription = "Crie itens, abra caixas e raspe stickers - organize e planeje o inventário dos seus sonhos em nossos servidores CS2";
   const appTitle = "ZK Servidores™ | Skinchanger";
-  const appWideImage = `${appUrl}/images/skinchanger-preto.png`;
+  const appWideImage = `${appUrl}/images/skinchanger.png`;
 
   return [
     {
