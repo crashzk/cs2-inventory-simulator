a7c704e62b8b62ca16de7bcaad3a60ef4cae2fa2
crashzk <contato@zkservidores.com>
2024-05-14 22:20:48 -0300

    Update donate-header-link.tsx

--- app/components/donate-header-link.tsx
+++ app/components/donate-header-link.tsx
@@ -3,7 +3,7 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-import { faGratipay } from "@fortawesome/free-brands-svg-icons";
+import { faStar } from "@fortawesome/free-brands-svg-icons";
 import { useTranslate } from "./app-context";
 import { HeaderLink } from "./header-link";
 
@@ -18,7 +18,7 @@ export function DonateHeaderLink() {
     ) && (
       <HeaderLink
         className="font-bold"
-        icon={faGratipay}
+        icon={faStar}
         label={translate("HeaderDonate")}
         target="_blank"
         to="https://vip.zkservidores.com"
