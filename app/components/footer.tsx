196d72ceb623ebd22333725d254b15cc0842a5f6
crashzk <contato@zkservidores.com>
2024-12-21 17:55:49 -0300

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -17,7 +17,7 @@ export function Footer() {
     <footer className="my-8 select-none text-sm text-neutral-400 drop-shadow-sm">
       <div className="text-center">
         <span>
-          &copy; {new Date().getFullYear()}{" "}
+          &copy; 2017 {new Date().getFullYear()}{" "}</br>
           {appFooterName || DEFAULT_APP_FOOTER_NAME}
         </span>
       </div>
